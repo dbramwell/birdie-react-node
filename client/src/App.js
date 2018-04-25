9fda796cafcb9424a14c3953fafe83557acd6b9d
bram <david.j.bramwell@googlemail.com>
2018-04-25 15:07:57 +0100

    Added initial gui with test for pre-loading columns from DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {columns: []}
+
+  componentDidMount() {
+    return fetch('/columns')
+      .then(res => res.json())
+      .then(columns => this.setState({ columns }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Variable: </h1>
+        <select>
+          {this.state.columns.map((column, index) =>
+            <option key={index} value={column}>{column}</option>
+          )}
+        </select>
       </div>
     );
   }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
+beforeEach(() => {
+  fetch.resetMocks();
+  fetch.once(JSON.stringify(['col1', 'col2']));
+})
+
+it('app loads columns from api after mounting and sets them to options', async () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const app = shallow(<App />)
+  expect(app.state().columns).toEqual([]);
+  expect(app.find('option').length).toBe(0);
+  fetch.once(JSON.stringify(['col1', 'col2']))
+  await app.instance().componentDidMount();
+  expect(fetch.mock.calls[0][0]).toEqual('/columns')
+  app.update();
+  expect(app.state().columns).toEqual(['col1', 'col2']);
+  expect(app.find('option').length).toBe(2);
 });
