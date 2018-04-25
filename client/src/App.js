548eef703e3cfcb8b72d4b0c7a290bfb9f23fc45
bram <david.j.bramwell@googlemail.com>
2018-04-25 16:24:45 +0100

    Added table and display of N/A for null values

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  
-  state = {columns: []}
+
+  constructor(props) {
+    super(props);
+    this.state = {columns: [], data: []}
+    this.onSelectChange = this.onSelectChange.bind(this);
+  }
 
   componentDidMount() {
     return fetch('/columns')
@@ -11,15 +15,39 @@ class App extends Component {
       .then(columns => this.setState({ columns }));
   }
 
+  setTableData(column) {
+    return fetch(`/columns/${column}`)
+      .then(res => res.json())
+      .then(data => this.setState({ data }));
+  }
+
+  onSelectChange(event) {
+    this.setTableData(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Variable: </h1>
-        <select>
+        <select onChange={this.onSelectChange} >
           {this.state.columns.map((column, index) =>
             <option key={index} value={column}>{column}</option>
           )}
         </select>
+        <table>
+          <tr>
+            <th>Value</th>
+            <th>Count</th>
+            <th>Average Age</th>
+          </tr>
+          {this.state.data.map((d, index) =>
+            <tr key={index}>
+              <td className='value'>{d.value || 'N/A' }</td>
+              <td className='count'>{d.count}</td>
+              <td className='average-age'>{d['average age'] || 'N/A'}</td>
+            </tr>
+          )}
+        </table>
       </div>
     );
   }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
@@ -9,7 +8,6 @@ beforeEach(() => {
 })
 
 it('app loads columns from api after mounting and sets them to options', async () => {
-  const div = document.createElement('div');
   const app = shallow(<App />)
   expect(app.state().columns).toEqual([]);
   expect(app.find('option').length).toBe(0);
@@ -19,4 +17,32 @@ it('app loads columns from api after mounting and sets them to options', async (
   app.update();
   expect(app.state().columns).toEqual(['col1', 'col2']);
   expect(app.find('option').length).toBe(2);
+});
+
+it('setTableData loads column data from the api and sets to state', async () => {
+  const sexData = [
+    {value: 'Male', count: 5, 'average age': 30.5},
+    {value: 'Female', count: 6, 'average age': 29.5}
+  ];
+  const app = shallow(<App />);
+  expect(app.state().data).toEqual([]);
+  fetch.once(JSON.stringify(sexData));
+  await app.instance().setTableData('sex');
+  expect(fetch.mock.calls[1][0]).toEqual('/columns/sex');
+  expect(app.state().data).toEqual(sexData);
+});
+
+it('table on page is populated with data from state', () => {
+  const app = shallow(<App />);
+  app.setState({data: [{value: 'Male', count: 5, 'average age': 50}]});
+  expect(app.find('table td.value').text()).toBe('Male');
+  expect(app.find('table td.count').text()).toBe('5');
+  expect(app.find('table td.average-age').text()).toBe('50');
+});
+
+it('if values are null then show N/A in table', () => {
+  const app = shallow(<App />);
+  app.setState({data: [{value: null, count: 5, 'average age': null}]});
+  expect(app.find('table td.value').text()).toBe('N/A');
+  expect(app.find('table td.average-age').text()).toBe('N/A');
 });
