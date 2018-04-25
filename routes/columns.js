7d588b768dfa1d17106c02bfeca6d18be958bcc8
bram <david.j.bramwell@googlemail.com>
2018-04-25 15:29:00 +0100

    Add db request for column info

--- routes/columns.js
+++ routes/columns.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../db/db');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   db.serialize(() => {
     db.all(`PRAGMA table_info(census_learn_sql)`, (err, columns) => {
       if (err) {
@@ -11,6 +11,24 @@ router.get('/', function(req, res, next) {
       } else {
         let columnNames = columns.map(column => column.name);
         res.send(columnNames);
+      }
+    });
+  });
+});
+
+router.get('/:column/', function(req, res, next) {
+  db.serialize(() => {
+    db.all(`SELECT distinct(${req.params.column}) as value,
+                   count(*) as count,
+                   avg(age) as 'average age'
+            from census_learn_sql
+            group by ${req.params.column}
+            order by count desc
+            limit 100;`, (err, columns) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        res.send(columns);
       }
     });
   });
